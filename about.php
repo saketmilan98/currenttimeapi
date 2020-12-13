ddb580ddcca6a602a39123aec23e7a6eefd69f58
Saket Milan <saketmilan77@gmail.com>
2020-12-13 16:59:18 +0530

    final commit

--- about.php
+++ about.php
@@ -1,12 +1,5 @@
 <?php
-//echo "welcome to this app";
-
-//date_default_timezone_set('Australia/Melbourne');
-//$date1 = date('m/d/Y h:i:s a', time());
-
 date_default_timezone_set('Asia/Kolkata');
-//$date1 = date('m/d/Y h:i:s a');
 $results = json_encode(array('time' => date('d-m-Y H:i:s')), true);
-//json_encode($date1);
 echo $results;
 ?>
