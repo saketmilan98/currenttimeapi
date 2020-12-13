158175bb312c80b24a1bd606da8a7304564a7714
Saket Milan <saketmilan77@gmail.com>
2020-12-13 16:53:59 +0530

    test2

--- about.php
+++ about.php
@@ -5,7 +5,8 @@
 //$date1 = date('m/d/Y h:i:s a', time());
 
 date_default_timezone_set('Asia/Kolkata');
-$date1 = date('m/d/Y h:i:s a');
+//$date1 = date('m/d/Y h:i:s a');
+$results = json_encode(array('status' => 'ok', 'time' => date('m/d/Y H:i:s')), true);
 //json_encode($date1);
-echo $date1;
+echo $results;
 ?>
