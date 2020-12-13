9e7932ddf94a0c3bb0d0f14ac6d3329bfc5bceab
Saket Milan <saketmilan77@gmail.com>
2020-12-13 16:47:51 +0530

    test2

--- about.php
+++ about.php
@@ -4,8 +4,8 @@
 //date_default_timezone_set('Australia/Melbourne');
 //$date1 = date('m/d/Y h:i:s a', time());
 
-date_default_timezone_set('Australia/Melbourne');
-$date1 = date('m/d/Y h:i:s a', time());
-//json_encode(date1);
-echo $date1;
+date_default_timezone_set('Asia/Kolkata');
+$date1 = date('m/d/Y h:i:s a');
+json_encode($date1);
+//echo $date1;
 ?>
