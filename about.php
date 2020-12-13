702c062fa4b09f39eee2bcb5dfcc9fd71528347f
Saket Milan <saketmilan77@gmail.com>
2020-12-13 16:39:52 +0530

    test2

--- about.php
+++ about.php
@@ -1,6 +1,7 @@
 <?php
 echo "welcome to this app";
 date_default_timezone_set('Australia/Melbourne');
-$date = date('m/d/Y h:i:s a', time());
+$date1 = date('m/d/Y h:i:s a', time());
+json_encode($date1)
 echo date
 ?>
